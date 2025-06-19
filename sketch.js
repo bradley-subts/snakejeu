4e8d792fe264d0abd246880f81c09cf4f4507a18
bradley-subts <kq5u.epic@gmail.com>
2025-06-19 16:01:53 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -201,3 +201,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+window.addEventListener("keydown", function(e) {
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
